9ad6127cec587c578926bb0444fe420cc185c082
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-02-07 16:07:39 +0530

    [Swathi]:Added Indian States Census Analyser Prgram

---
+++ IndianStatesCensusAnalyserProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianStatesCensusAnalyserProblem
+{ 
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Displaying Indian States Census Analyser Problem");
+        }
+    }
+}
